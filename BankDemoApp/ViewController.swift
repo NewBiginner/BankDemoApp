ddc9cbedb28d66d932d796619fdf9dc4c7fcd25c
David Wu <dvdw77@gmail.com>
2026-04-22 09:21:01 +0800

    Initial Commit

---
+++ BankDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankDemoApp
+//
+//  Created by David on 2026/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BankDemoAppTests/BankDemoAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  BankDemoAppTests.swift
+//  BankDemoAppTests
+//
+//  Created by David on 2026/4/22.
+//
+
+import Testing
+@testable import BankDemoApp
+
+struct BankDemoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
